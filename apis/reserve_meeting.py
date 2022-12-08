9c4f353e2a6a790b8a1cc1d2c1fa3906d5874a41
NeginR <70764388+NeginR@users.noreply.github.com>
2022-12-09 00:26:30 +0330

    add information for resereve meeting's api

---
+++ apis/reserve_meeting.py
@@ -0,0 +1,18 @@
+import json
+
+def show_mh_day_meetings(mh_id): 
+    pass #zahra side
+    
+def remove_mh_available_time(mh_id, time_id): 
+    pass #zahra side
+    
+def append_mh_meetings(mh_id, time_id): 
+    pass #zahra side
+
+def append_user_meetings(mh_id, time_id): #append a mh_time and return its id
+    pass #zahra side
+
+input = '''{ 
+    "mh_id":"123", 
+    "start_day":[{"date":"12/08/2022", "meetings":[{"start_time":"12:30", "end_time"="13:30"}, {"start_time":"17:30", "end_time"="18:30"}]}
+        '''
