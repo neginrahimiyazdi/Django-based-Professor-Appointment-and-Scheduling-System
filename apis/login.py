8fa7c3c849a6823369ff7a1ed2e829839d92c653
Abolfazl A.Ansari <77450405+AbolfazlAAnsari@users.noreply.github.com>
2022-12-09 01:45:21 +0330

    login api

---
+++ apis/login.py
@@ -0,0 +1,15 @@
+import json
+
+def is_student(username, password): 
+    # check this username is valid or not
+    # check the correction of password
+    # zahra side
+    # database side
+    # return 1,0
+    pass
+
+
+input = '''{ 
+    "username":"abolfazlansari@aut.ac.ir",
+    "password":"Icannontsharemypass",
+        }'''
