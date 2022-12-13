d20447ba70a8414376da42341b921986b7740b49
NeginR <70764388+NeginR@users.noreply.github.com>
2022-12-13 21:31:45 +0330

    add mh fill time table

--- apis/mh_fill_timetable.py
+++ apis/mh_fill_timetable.py
@@ -9,6 +9,7 @@ def append_time(date, start_time, end_time): #append a time and return its id
 def append_mh_time(mh_id, time_id): #append a mh_time and return its id
     pass #zahra side
 
+
 input = '''{ 
     "mh_id":"123", 
     "days":[{"date":"12/08/2022", "meetings":[{"start_time":"12:30", "end_time"="13:30"}, {"start_time":"17:30", "end_time"="18:30"}]}
