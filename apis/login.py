47c7423d6dc30f2640baf23e43afc6bcef30005a
AbolfazlAAnsari <abolfazlansari.aut@gmail.com>
2022-12-14 03:44:09 +0330

    login api done and related comments mentioned

--- apis/login.py
+++ apis/login.py
@@ -1,17 +1,55 @@
+# follow up codes
+
+# in main -> urls 
+#   from django.contrib.auth import views as auth_views
+#   add:
+#   path('login/', auth_views.LoginView.as_view(), name='Login'),
+#   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+# Note: after login sucessfuly go to the setting.py and
+# add:
+#   LOGIN_REDIRECT_URL = 'blog-home'
+
+# Note: We need log out too, because of security problem!
+# When you login the user can access admin!
+
+from django.contrib.auth import views as auth_views
 import json
-
-def is_student(username, password): 
-    # check this username is valid or not
-    # check the correction of password
-    # zahra side
-    # database side
-    # return 1,0 
-    pass
-
-# We do not have MH login at this point
-
 
 input = '''{ 
     "username":"abolfazlansari@aut.ac.ir",
     "password":"Icannontsharemypass",
         }'''
+
+# def is_student(username, password): -> it breaks to get_password
+    # check this username is valid or not
+    # check the correction of password
+    # zahra side
+    # database side
+    # return 1,0 
+    # pass
+
+# We do not have MH login at this point
+
+
+
+# The process of checking username and password
+# get Pass from database
+
+def get_password(username:'str') -> 'str':
+    # Send username to database
+    if (user_not_found_in_database):
+        return 'username not found!'
+        #It could be a good idea to redirect register 
+    else:
+        return password
+
+
+database_password = get_password(username)
+
+
+if database_passowrd == password:
+    print("Login sucessfully.")
+else:
+    print("Username or Password is incorrect.")
+
