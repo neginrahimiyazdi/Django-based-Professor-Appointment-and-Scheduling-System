a17593d8ccaf24a6c4edad2039e66d3abaa87544
AbolfazlAAnsari <abolfazlansari.aut@gmail.com>
2022-12-15 20:26:55 +0330

    database configuration with liara postgresql

--- bia2django/bia2django/settings.py
+++ bia2django/bia2django/settings.py
@@ -72,11 +72,16 @@ WSGI_APPLICATION = 'bia2django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'postgres',
+'USER': 'root',
+'PASSWORD': 'o9HSiBsc5iCeOo7WGYGurYBu',
+'HOST': 'may.iran.liara.ir',
+'PORT': '31860',
+}
 }
 
 
