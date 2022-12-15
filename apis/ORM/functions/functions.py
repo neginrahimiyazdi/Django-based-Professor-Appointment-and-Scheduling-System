f35d03ddc407e7292a2de6d695129ca46e72999f
nekoouu <120061159+nekoouu@users.noreply.github.com>
2022-12-15 19:42:29 +0330

    Add files via upload
    model and ORM functions

---
+++ apis/ORM/functions/functions.py
@@ -0,0 +1,29 @@
+import datetime
+from testdb.models import Time,User,MH,Meeting,MH_Time
+#?
+def remove_mh_day_meetings(mh_id, date):  # remove * from meetings where mh_id={} and date={}
+    queryset = Time.objects.filter(date=datetime.date(date)).values('time_id')
+    try :
+        records = Meeting.objects.get(mh_id = mh_id , time_id= queryset)
+        records.delete()
+        print("Records deleted successfully!")
+    except :
+        print("Records doesn't exist")
+    pass  # zahra side
+
+
+def append_time(date, start_time, end_time):  # append a time and return its id
+    # time(year = 0, month = 0, day = 0)
+    # time(hour = 0, minute = 0, second = 0)
+    t = Time(date= datetime.date(date),start_time= datetime.time(start_time),end_time= datetime.time(end_time))
+    t.save()
+    return t.time_id
+    # zahra side
+
+
+def append_mh_time(mh_id, time_id):  # append a mh_time and return its id
+    mt = MH_Time(mh_id= mh_id,time_id=time_id)
+    mt.save()
+    return mt.id
+    # zahra side
+
---
+++ apis/ORM/models.py
@@ -0,0 +1,87 @@
+from django.db import models
+from django.db import migrations
+import datetime
+
+
+# Create your models here.
+class Time(models.Model):
+    time_id = models.BigAutoField(primary_key=True)
+    date =  models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return "%s %s" % (self.date, self.start_time)
+
+class MH(models.Model):
+    MH_id = models.BigAutoField(primary_key=True)
+    first_name = models.CharField(max_length= 100)
+    last_name = models.CharField(max_length= 100)
+    MH_email = models.EmailField()
+    teacher_number = models.IntegerField()
+    degree = models.CharField(max_length= 100)
+    field = models.CharField(max_length= 300)
+    link_to_webpage = models.CharField(max_length= 1000)
+    #QR = models.
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+class User(models.Model):
+    user_id = models.BigAutoField(primary_key=True)
+    first_name = models.CharField(max_length= 100)
+    last_name = models.CharField(max_length= 100)
+    user_email = models.EmailField()
+    student_number = models.IntegerField()
+    mobile_number = models.CharField(max_length= 15)
+    degree = models.CharField(max_length= 100)
+    field = models.CharField(max_length= 300)
+    university = models.CharField(max_length= 200)
+    #adviser_id = models.IntegerField()
+    #guider_id = models.IntegerField()
+    adviser_id = models.ForeignKey(MH, on_delete=models.CASCADE)
+    #guider_id = models.ForeignKey(MH, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+class Meeting(models.Model):
+    meeting_id = models.BigAutoField(primary_key=True)
+    #MH_id = models.IntegerField()
+    #user_id = models.IntegerField()
+    #time_id = models.IntegerField()
+    subject = models.CharField(max_length= 500)
+    rate = models.IntegerField()
+    description = models.TextField()
+    was_holded = models.BooleanField()
+    MH_id = models.ForeignKey(MH, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_id = models.ForeignKey(Time, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s" % (self.meeting_id)
+
+class MH_Time(models.Model) :
+    id = models.BigAutoField(primary_key=True)
+    #MH_id = models.IntegerField()
+    #time_id = models.IntegerField()
+    MH_id = models.ForeignKey(MH, on_delete=models.CASCADE)
+    time_id = models.ForeignKey(Time, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s" % (self.id)
+################CREATING OBJECTS#################
+date_list=[]
+for i in range(0,5):
+    # time(year = 0, month = 0, day = 0)
+    date_list.append(datetime.date(1997, 10, 19))
+start_time_list=[]
+end_time_list=[]
+for i in range(0,5):
+    # time(hour = 0, minute = 0, second = 0)
+    start_time_list.append(datetime.time(8+i,0,0))
+    end_time_list.append(datetime.time(9+i,0,0))
+
+for i in range(0,5) :
+    t = Time(date=date_list[i],start_time=start_time_list[i],end_time=end_time_list[i])
+    t.save()
